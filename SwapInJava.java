e1f6cb043367bfb392e182a522fc7b69e4350be5
FIRE-MOS <63502295+FIRE-MOS@users.noreply.github.com>
2020-04-11 21:33:56 +0800

    Additional Exolanation
    Because I am lazy,So I didnt create getter and setter in NUM ,lazy enough to  make a and b public

---
+++ SwapInJava.java
@@ -0,0 +1,21 @@
+Class NUM{
+ public int a;//miss int
+ public int b;
+}
+Public Class SwapTest{
+  public static swaptest(Num num){//miss static ,because it is used in static method
+     int temp;
+     temp=num.a;
+     num.a=num.b;
+     num.b=temp;
+  }
+  public static void main(String[] args){
+    Num num=new Num();
+    num.a=6;
+    num.b=9;
+    SwapTest st=new SwapTest();//dont need to generate a object ,because it is inside class SwapTest
+    st.swaptest(num);
+    System.out.println(num.a);
+    System.out.println(num.b);
+  }
+}
